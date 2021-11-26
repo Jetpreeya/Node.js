7c18042e23ad45be80167fad4fa3468a44bb51b4
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2021-11-26 16:58:04 +0100

    My homework for week2

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+
+setTimeout(function(){
+    console.log('Hello');
+},2000
+);
+
+setTimeout(function(){
+    console.log('Hello again');
+},3000
+);
